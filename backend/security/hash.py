5f626fee5f48b48f167415b256b3cb38a73054a1
Christianymoon <chrisvergara7@outlook.com>
2026-02-16 23:13:01 -0600

    security core initialized

---
+++ backend/security/hash.py
@@ -0,0 +1,9 @@
+from hashlib import sha256
+
+SALT = "alejandria"
+
+def hash_password(password: str) -> str:
+    return sha256(SALT + password).hexdigest()
+
+def verify_password(password: str, hash: str) -> bool:
+    return hash_password(password) == hash
